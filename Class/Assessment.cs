13a47f61727795468b163291920b381d5e13326a
AkmzZZ <akmzzznra@gmail.com>
2021-06-28 20:06:58 +0600

    first commit

---
+++ Class/Assessment.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace University_hierarchy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
