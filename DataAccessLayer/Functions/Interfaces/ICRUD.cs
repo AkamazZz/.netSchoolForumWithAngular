33289003e708f45ddd1a8aaa6af16dc269c8a705
AkmzZZ <akmzzznra@gmail.com>
2021-07-01 14:59:09 +0600

    GRUD is added

---
+++ DataAccessLayer/Functions/Interfaces/ICRUD.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Functions.Interfaces
+{
+    public interface ICRUD
+    {
+        Task<T> Create<T>(T objectForDb) where T : class;
+        Task<T> Read<T>(int entityId) where T : class;
+        Task<List<T>> ReadAll<T>() where T : class;
+        Task<T> Update<T>(T objectToUpdate, int entityId) where T : class;
+        Task<bool> Delete<T>(int entityId) where T : class;
+    }
+}
